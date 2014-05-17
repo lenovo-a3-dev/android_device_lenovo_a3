ce45f6ab130085664a4343c9179f163abd2d3a10
Olivier <y.karasangabo@bg-rams.ac.at>
2014-05-18 00:12:40 +0200

    a3: add some build props

--- cm.mk
+++ cm.mk
@@ -20,6 +20,9 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 TARGET_SCREEN_HEIGHT := 1024
 TARGET_SCREEN_WIDTH := 600
 
+# Build fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LenovoA3000-H BUILD_FINGERPRINT=Lenovo/LenovoA3000-H/A3000:4.2.2/JDQ39/A3000_A422_011_022_140127_WW_C:user/release-keys PRIVATE_BUILD_DESC="a3000_row_call-user 4.2.2 JDQ39 147 release-keys "
+
 # Overrides for CM
 PRODUCT_NAME := cm_a3
 PRODUCT_DEVICE := a3
