db94096dabf79724579c5cd6894ef2a0733777c3
Kashif Minhaj <dailypctips@gmail.com>
2014-10-12 09:15:15 -0400

    a3: nvram permission error fix
    Change-Id: Ic26666f0ca7227f46b3ca6c0a526275b2aff159d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,4 +88,14 @@ WIFI_DRIVER_FW_PATH_STA := "STA"
 WIFI_DRIVER_FW_PATH_P2P := "STA+P2P"
 
 # telephony
+<<<<<<< HEAD
 BOARD_RIL_CLASS := ../../../device/lenovo/a3/ril/
+=======
+BOARD_RIL_CLASS := ../../../device/micromax/a116/ril/
+
+#nvram permission fix
+TARGET_SPECIFIC_HEADER_PATH := device/micromax/a116/include
+
+# allow more than one lun file
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
+>>>>>>> 631f098... a116: nvram permission error fix
