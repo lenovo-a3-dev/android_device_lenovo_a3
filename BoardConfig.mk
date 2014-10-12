f5c6750db836f89bedab8cf8115c1db8ee6a9800
Kashif Minhaj <dailypctips@gmail.com>
2014-10-12 09:15:15 -0400

    a3: nvram permission error fix
    Change-Id: Ic26666f0ca7227f46b3ca6c0a526275b2aff159d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,3 +89,6 @@ WIFI_DRIVER_FW_PATH_P2P := "STA+P2P"
 
 # telephony
 BOARD_RIL_CLASS := ../../../device/lenovo/a3/ril/
+
+# nvram permission fix
+TARGET_SPECIFIC_HEADER_PATH := device/lenovo/a3/include
